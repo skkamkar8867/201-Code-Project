eeb1b2e2cb210ae1fb437f838135cb56a0061fce
Sushant Kamkar <skkamkar@amazon.com>
2019-02-20 22:48:15 -0800

    Final Submission for 201 assignment

--- js/app.js
+++ js/app.js
@@ -6,6 +6,10 @@
 	checkreadyflag(readyflag);
 	var age = getage();
 	checkage(age);
+	var married = getmarried();
+	checkage(married);
+	var kid = getkid();
+	checkage(kid);
 
 	alert('Game Over!!!');
 
@@ -23,27 +27,6 @@ function getreadyflag() {
 		return readyflag;
 };
 
-/* The function prompts user to enter age */
-function getage() {
-	age = window.prompt('Can you guess my age');
-	return age;
-};
-
-
-/* The function checks validates the age provided by user */
-function checkage(age){
-	age = this.age;
-	var correctage = '50';
-	if (age === null) {
-		alert('No User Input. Thanking for Playing')
-	}
-	else if (age === correctage) {
-		alert('Congratulations!! '+ age +' is correct answer.');
-	}
-	else {
-		alert('Ops!! '+ age +' is incorrect answer.Please try the game again');
-	}
-};
 
 /* The function checks if user is interested in playing */
 function checkreadyflag(readyflag){
@@ -62,3 +45,71 @@ function checkreadyflag(readyflag){
 		alert('Please enter yes or y to play game');
 	}
 };
+
+/* The function prompts user to enter age */
+function getage() {
+	age = window.prompt('Can you guess my age');
+	return age;
+};
+
+
+function getmarried() {
+	age = window.prompt('Am i married');
+	return married;
+};
+
+function getkid() {
+	age = window.prompt('Do you think i have kid');
+	return kid;
+};
+
+
+/* The function checks validates the age provided by user */
+function checkage(age){
+	age = this.age;
+	var correctage = '50';
+	if (age === null) {
+		alert('No User Input. Thanking for Playing')
+	}
+	else if (age === correctage) {
+		alert('Congratulations!! '+ age +' is correct answer.');
+	}
+	else {
+		alert('Ops!! '+ age +' is incorrect answer.Please try the game again');
+	}
+};
+
+/* The function checks validates the age provided by user */
+function checkmarried(married){
+	married = this.married;
+  	married = married.toLowerCase();
+	var expectedvalue1 = 'y';
+  var expectedvalue2 = 'yes';
+
+	if (married === null) {
+		alert('No User Input. Thanking for Playing')
+	}
+	else if (married === expectedvalue1 || married === expectedvalue2) {
+		alert('Congratulations!! your is answer is correct');
+	}
+	else {
+		alert('Ops!! that is incorrect answer');
+	}
+};
+
+/* The function checks validates the age provided by user */
+function checkkid(kid){
+	kid = this.kid;
+	var correctkid = 'y';
+	if (kid === null) {
+		alert('No User Input. Thanking for Playing')
+	}
+	else if (kid === correctkid) {
+		alert('Congratulations!! your is answer is correct');
+	}
+	else {
+		alert('Ops!! that is incorrect answer');
+	}
+};
+
+
